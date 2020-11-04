d59b02649ac7156794f2c31e560efe143c42154e
Tom Hawkins <tks.hawkins11@gmail.com>
2020-11-04 15:06:17 +1000

    Window

---
+++ win32_platform.cpp
@@ -0,0 +1,63 @@
+#include <Windows.h>
+
+bool running = true;
+
+LRESULT CALLBACK window_callback(
+	HWND   hwnd,
+	UINT   uMsg,
+	WPARAM wParam,
+	LPARAM lParam) 
+{
+	LRESULT result = 0;
+	switch (uMsg)
+		{
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			running = false;
+		} break;
+
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+	return result;
+	
+}
+
+int WinMain(
+	HINSTANCE hInstance, 
+	HINSTANCE hPrevInstance, 
+	LPSTR	  lpCmdLine, 
+	int		  nShowCmd) {
+
+	// create a window class
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpszClassName = L"Game Window";
+	window_class.lpfnWndProc = window_callback;
+
+	// register the class
+	RegisterClass(&window_class);
+
+	// create the window
+	HWND window = CreateWindow(window_class.lpszClassName, 
+				L"My First Game", 
+				WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
+				CW_USEDEFAULT, CW_USEDEFAULT, 
+				1280, 720,
+				0, 0, 
+				hInstance, 0);
+
+	while (running)
+	{
+		//Input
+		MSG message; 
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+		//Simulate
+
+		//render
+	}
+}
