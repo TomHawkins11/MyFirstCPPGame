a44b1f6d7027046f09a60c158689c0f621ab17c9
Tom Hawkins <tks.hawkins11@gmail.com>
2020-11-05 19:56:57 +1000

    Drawing Graphics 0.22
    Window Working properly

--- win32_platform.cpp
+++ win32_platform.cpp
@@ -1,10 +1,15 @@
+
 #include <Windows.h>
 
 bool running = true;
-void* buffer_memory;
-int buffer_width;
-int buffer_height;
-BITMAPINFO buffer_bitmap_info;
+struct Render_State {
+	int height, width;
+	void* memory;
+
+	BITMAPINFO bitmap_info;
+};
+
+Render_State render_state;
 
 LRESULT CALLBACK window_callback(
 	HWND   hwnd,
@@ -22,21 +27,20 @@ LRESULT CALLBACK window_callback(
 		case WM_SIZE: {
 			RECT rect;
 			GetClientRect(hwnd, &rect);
-			buffer_width = rect.right - rect.left;
-			buffer_height = rect.top - rect.bottom;
+			render_state.width = rect.right - rect.left;
+			render_state.height = rect.bottom - rect.top;
 
-			int buffer_size = buffer_width * buffer_height * sizeof(unsigned int);
+			int size = render_state.width * render_state.height * sizeof(unsigned int);
 
-			if (buffer_memory) VirtualFree(buffer_memory, 0, MEM_RELEASE);
-			buffer_memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+			render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
-			buffer_bitmap_info.bmiHeader.biSize = sizeof(buffer_bitmap_info.bmiHeader);
-			buffer_bitmap_info.bmiHeader.biWidth = buffer_width;
-			buffer_bitmap_info.bmiHeader.biHeight = buffer_height;
-			buffer_bitmap_info.bmiHeader.biPlanes = 1;
-			buffer_bitmap_info.bmiHeader.biBitCount = 32;
-			buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
-
+			render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+			render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+			render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+			render_state.bitmap_info.bmiHeader.biPlanes = 1;
+			render_state.bitmap_info.bmiHeader.biBitCount = 32;
+			render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
 		} break;
 
 		default: {
@@ -56,7 +60,7 @@ int WinMain(
 	// create a window class
 	WNDCLASS window_class = {};
 	window_class.style = CS_HREDRAW | CS_VREDRAW;
-	window_class.lpszClassName = L"Game Window";
+	window_class.lpszClassName = L"Game Window Class";
 	window_class.lpfnWndProc = window_callback;
 
 	// register the class
@@ -85,6 +89,6 @@ int WinMain(
 
 
 		//render
-		StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, buffer_width, buffer_height, buffer_memory, &buffer_bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
 	}
 }
