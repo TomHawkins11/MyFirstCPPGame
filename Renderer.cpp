4d2477896f4df1f76bd9e0aaa9fea5ce1b15faa4
Tom Hawkins <tks.hawkins11@gmail.com>
2020-11-08 16:02:17 +1000

    DG 0.9
    nearly done

--- Renderer.cpp
+++ Renderer.cpp
@@ -1,10 +1,41 @@
-void render_background() {
-	unsigned int* pixel = (unsigned int*)render_state.memory;
+internal void 
+clear_screen(u32 color) {
+	u32* pixel = (u32*)render_state.memory;
 	for (int i = 0; i < render_state.height; i++)
 	{
 		for (int j = 0; j < render_state.width; j++)
 		{
-			*pixel++ = i * j;
+			*pixel++ = color;
 		}
 	}
+}
+
+
+internal void 
+draw_rect_in_pixels(int x0, int y0, int x1, int y1, u32 color) {
+	x0 = clamp(0, x0, render_state.width);
+	x1 = clamp(0, x1, render_state.width);
+	y0 = clamp(0, y0, render_state.height);
+	y1 = clamp(0, y1, render_state.height);
+
+	for (int y = y0; y < y1; y++)
+	{
+		u32* pixel = (u32*)render_state.memory + x0 + y* render_state.width;
+		for (int x = x0; x < x1; x++)
+		{
+			*pixel++ = color;
+		}
+	}
+}
+
+internal void
+draw_rect(float x, float y, float half_size_x, float half_size_y, u32 color) {
+	//change to pixels
+	int x0 = x - half_size_x;
+	int x1 = x - half_size_x;
+	int x0 = x - half_size_x;
+	int x0 = x - half_size_x;
+
+
+	draw_rect_in_pixels(x0, y0, x1, y1, color)
 }
--- win32_platform.cpp
+++ win32_platform.cpp
@@ -1,7 +1,8 @@
 
+#include "utils.cpp"
 #include <Windows.h>
 
-bool running = true;
+global_variable bool running = true;
 struct Render_State {
 	int height, width;
 	void* memory;
@@ -9,7 +10,8 @@ struct Render_State {
 	BITMAPINFO bitmap_info;
 };
 
-Render_State render_state;
+global_variable Render_State render_state;
+#include "Renderer.cpp"
 
 LRESULT CALLBACK window_callback(
 	HWND   hwnd,
@@ -30,7 +32,7 @@ LRESULT CALLBACK window_callback(
 			render_state.width = rect.right - rect.left;
 			render_state.height = rect.bottom - rect.top;
 
-			int size = render_state.width * render_state.height * sizeof(unsigned int);
+			int size = render_state.width * render_state.height * sizeof(u32);
 
 			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
 			render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
@@ -86,9 +88,12 @@ int WinMain(
 		}
 
 		//Simulate
-		render_background();
+		clear_screen(16432170);
+		draw_rect_in_pixels(50, 50, 200, 500, 15749989);
 
 		//render
-		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, 
+						render_state.width, render_state.height, render_state.memory, 
+						&render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
 	}
 }
